c7e824ef1c40e6dfaea6fdbdce40b659820e387c
kphacko <karan2000patil@gmail.com>
2020-02-14 19:24:18 +0530

    Payment gateway files added

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$keyId = 'rzp_test_dMPYNsPVZ07oa3';
+$keySecret = 'kT7zm6XBXebZQlcIDNcizIzy';
+$displayCurrency = 'INR';
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to config.php to report any errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
